b64fa1c7524743b7f27f33bcc1e6bdf15fe13574
HugoDeArgila <hugo.deargila@tajamar365.com>
2024-11-14 09:40:47 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-
-sudo apt update && apt upgrade
-sudo apt install openjdk-8-jdk
-sudo apt install python3
-
-cd /opt/spark
+ 
+cd ~
+ 
+sudo apt update -y && apt upgrade -y
+sudo apt install openjdk-8-jdk -y
+sudo apt install python3 -y
+ 
 sudo wget -N https://downloads.apache.org/spark/spark-3.5.3/spark-3.5.3-bin-hadoop3.tgz
 sudo tar -xvf spark-3.5.3-bin-hadoop3.tgz
-
-cd ~
-echo 'export SPARK_HOME=/opt/spark' >> ~/.bashrc
-echo 'export PATH=$SPARK_HOME/bin:$PATH' >> ~/.bashrc
-echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> ~/.bashrc
-echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
-
-source ~/.bashrc
-
-echo -e "versión java: $(java -version)\nversión python: $(python3 --version)"    
-spark-shell
+sudo mv spark-3.5.3-bin-hadoop3 /opt/spark
+sudo mv /opt/spark/spark-3.5.3-bin-hadoop3/* /opt/spark
  
+cd ~
+sudo echo "export SPARK_HOME=/opt/spark" >> ~/bashPrueba.txt
+sudo echo "export PATH=\$SPARK_HOME/bin:\$PATH" >> ~/bashPrueba.txt
+sudo echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/bashPrueba.txt
+sudo echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/bashPrueba.txt
+ 
+sudo cat ~/bashPrueba.txt >> ~/.bashrc
+rm ~/bashPrueba.txt
+ 
+sudo echo "#!/bin/bash" >> sour.sh
+sudo echo "source ~/.bashrc" >> sour.sh
+sudo chmod +x sour.sh
+sudo ./sour.sh
+sudo rm sour.sh
+ 
+echo -e "java version: $(java -version) \n python version: $(python3 --version)"
